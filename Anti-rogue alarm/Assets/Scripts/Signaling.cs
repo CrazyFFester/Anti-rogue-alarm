2cd97ac9975b8a9f34a16b8aa265bb0b6ca813f3
Albert Kun <60484804+CrazyFFester@users.noreply.github.com>
2021-11-11 18:45:12 +0300

    Update Signaling.cs

--- Anti-rogue alarm/Assets/Scripts/Signaling.cs
+++ Anti-rogue alarm/Assets/Scripts/Signaling.cs
@@ -1,34 +1,22 @@
 using System.Collections;
 using UnityEngine;
 
-<<<<<<< HEAD
 [RequireComponent(typeof(AudioSource))]
-
-public class Signaling : MonoBehaviour
-{
-    private AudioSource _alarmSound;
-    private bool _rogueInside;
-
-    private void Awake()
-    {
-        _alarmSound = GetComponent<AudioSource>(); 
-=======
+  
 public class Signaling : MonoBehaviour
 {
     private AudioSource alarmSound;
-    private bool rogueInside = false;
+    private bool rogueInside;
 
     private void Awake()
     {
         alarmSound = GetComponent<AudioSource>();   
->>>>>>> 32f1990e49e7f85095a30e1a78d774f9f94d52cf
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent<Rogue>(out Rogue rogue))
         {
-<<<<<<< HEAD
             if (!_rogueInside)
             {
                 StopCoroutine(ChangeSound(0f, 0.05f));
@@ -71,32 +59,7 @@ public class Signaling : MonoBehaviour
                 _alarmSound.volume = i;
                 yield return waitForSeconds;
             }
-=======
-            if (!rogueInside)
-            {
-                StopAllCoroutines();
-                rogueInside = true;
-                StartCoroutine(ChangeSound(1f, 0.025f));
-            }
-
-            else
-            {
-                StopAllCoroutines();
-                rogueInside = false;
-                StartCoroutine(ChangeSound(0f, -0.05f));
-            }
-        }
-    }
-     
-    private IEnumerator ChangeSound(float maxVolume, float scaleDivision)
-    {
-        WaitForSeconds waitForSeconds = new WaitForSeconds(0.1f);
-
-        for (float i = alarmSound.volume; i != maxVolume; i+= scaleDivision)
-        {
-            alarmSound.volume = i;
-            yield return waitForSeconds;
->>>>>>> 32f1990e49e7f85095a30e1a78d774f9f94d52cf
+            
         }
     }
 }
