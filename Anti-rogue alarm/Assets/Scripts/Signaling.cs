3d61c61c8e87c370b6a67a484e1d0d6833052967
Albert Kun <emir9596@gmail.com>
2022-04-30 00:53:19 +0300

    /

--- Anti-rogue alarm/Assets/Scripts/Signaling.cs
+++ Anti-rogue alarm/Assets/Scripts/Signaling.cs
@@ -6,40 +6,13 @@ using UnityEngine;
 public class Signaling : MonoBehaviour
 {
     private AudioSource _alarmSound;
-    private bool _rogueInside;
-    private Coroutine _onTriggerEnter;
-    private Coroutine _onTriggerExit;
-
+    
     private void Awake()
     {
         _alarmSound = GetComponent<AudioSource>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.TryGetComponent<Rogue>(out Rogue rogue))
-        {
-            if (!_rogueInside)
-            {
-                _onTriggerEnter = StartCoroutine(ChangeSound(1f, 0.025f));
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent<Rogue>(out Rogue rogue))
-        {
-            _rogueInside = !_rogueInside;
-            if (!_rogueInside)
-            {
-                StopCoroutine(_onTriggerEnter);
-                _onTriggerExit = StartCoroutine(ChangeSound(0f, 0.05f));
-            }
-        }
-    }
-
-    private IEnumerator ChangeSound(float targetVolume, float scaleDivision)
+    public IEnumerator ChangeSound(float targetVolume, float scaleDivision)
     {
         WaitForSeconds waitForSeconds = new WaitForSeconds(0.1f);
 
@@ -51,6 +24,7 @@ public class Signaling : MonoBehaviour
                 yield return waitForSeconds;
             }
         }
+
         else if (_alarmSound.volume > targetVolume)
         {
             while (_alarmSound.volume > targetVolume)
